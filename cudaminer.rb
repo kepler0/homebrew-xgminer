aaf75c724fa86a583a1430f378b5689f2b3cd6f2
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-01-19 12:30:42 -0700

    Changing the code where we must.
    http://grantammons.me/compiling-cudaminer-for-osx/

--- cudaminer.rb
+++ cudaminer.rb
@@ -5,6 +5,9 @@ class CudaMiner < Formula
   head 'https://github.com/cbuchner1/CudaMiner', :branch => 'master'
 
   def install
+    sed -i '46 s|#include <malloc.h>|#include <malloc/malloc.h>|' scrypt.cpp
+    sed -i '146 s|CUDA_LDFLAGS="-L$with_cuda/lib$SUFFIX"|CUDA_LDFLAGS="-L$with_cuda/lib"|' scrypt.cpp
+    sed -i '151 s|CUDA_LDFLAGS="-L/usr/local/cuda/lib$SUFFIX"|CUDA_LDFLAGS="-L/usr/local/cuda/lib|' scrypt.cpp
   end
 
   test do
